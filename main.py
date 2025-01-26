c6a6d32848fe22ff8761cdecc9bfc77f059142ef
MakAlex90 <meskalin4ik9@gmail.com>
2025-01-26 14:42:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Path
+
 
 app = FastAPI()
 
 @app.get("/")
 async def welcome():
-    return (f"Главная страница")
+    return 'Главная страница'
 
 @app.get('/user/admin')
 async def admin():
-    return (f'Вы вошли как администратор')
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def user(user_id):
-    return (f'Вы вошли как пользователь №{user_id}')
+async def user(user_id: Annotated[int,Path(gt=1, le=100, description='Enter User ID', example='1')]):
+    return f'Вы вошли как пользователь №{user_id}'
 
 
-@app.get('/user')
-async def user_info(username, age):
-    return f'Информация о пользователе, Имя: {username}, Возраст: {age}'
+@app.get('/user/{username}/{age}')
+async def user_info(username: Annotated[str,Path(min_length=5, max_length=20,
+                                      description='Enter username', example='UrbanUser')],
+                    age: Annotated[int,Path(gt=18, le=120, description='Enter age', example='24')]):
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
 
-# uvicorn main:app --reload (Для запуска сервера!)
+# uvicorn Scripts.main:app --reload (Для запуска сервера!)
